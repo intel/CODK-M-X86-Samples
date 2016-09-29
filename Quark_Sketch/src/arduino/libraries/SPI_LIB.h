d846d600206a01ca7b070d0ae91be1f65bea861d
Dino Tinitigan <bigdinotech@gmail.com>
2016-09-29 15:40:52 -0700

    Change license header of SPI library
    -change the license header of SPI library to the correct one which is
    Apache 2.0

--- Quark_Sketch/src/arduino/libraries/SPI_LIB.h
+++ Quark_Sketch/src/arduino/libraries/SPI_LIB.h
@@ -1,15 +1,17 @@
 /*
- * Copyright (c) 2010 by Cristian Maglie <c.maglie@arduino.cc>
- * Copyright (c) 2014 by Paul Stoffregen <paul@pjrc.com> (Transaction API)
- * Copyright (c) 2014 by Matthijs Kooijman <matthijs@stdin.nl> (SPISettings AVR)
- * Copyright (c) 2014 by Andrew J. Kroll <xxxajk@gmail.com> (atomicity fixes)
- * Copyright (c) 2016 by Intel Corporation <dino.tinitigan@intel.com (Curie ODK support)
- * SPI Master library for arduino.
+ * Copyright (c) 2016 Intel Corporation
  *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of either the GNU General Public License version 2
- * or the GNU Lesser General Public License version 2.1, both as
- * published by the Free Software Foundation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <zephyr.h>
