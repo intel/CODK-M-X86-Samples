2697a39cb90acefe03b4e1587ca27a95cbcf450e
Dino Tinitigan <bigdinotech@gmail.com>
2016-10-03 13:18:05 -0700

    pwm wrapper changes
    -modify pwm wrapper to be compatible with Zephyr 1.5
    -add pwmWriteResolution functionality

--- Quark_Sketch/src/arduino/wiring_analog.c
+++ Quark_Sketch/src/arduino/wiring_analog.c
@@ -30,18 +30,20 @@
 #include <pwm.h>
 #include <device.h>
 #include <misc/byteorder.h>
+#include <sys_clock.h>
 
 #include <pinmux.h>
 
-#define PWM_PERIOD 	65306	//490 Hz
-#define F_CPU 		32000000
-
+#define PWM_PERIOD 	2040	//490 Hz
+#define HW_CLOCK_CYCLES_PER_USEC  (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC / \
+				   USEC_PER_SEC)
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Standard Arduino PWM resolution */
 static int _writeResolution = 8;
+uint32_t maxResolutionValue = 0xFF;
 
 int pwm_period[4];
 
@@ -60,7 +62,8 @@ void analogInit()
 
 void analogWriteResolution(int res)
 {
-    _writeResolution = res;
+	_writeResolution = res;
+	maxResolutionValue = 0xFFFFFFFF >> (32-res);
 }
 
 static inline uint32_t mapResolution(uint32_t value, uint32_t from, uint32_t to)
@@ -77,14 +80,16 @@ void analogWrite(uint8_t pin, int val)
 {
 	pinMode(pin, OUTPUT);
 
-	int highPeriod = (val/255.0) * pwm_period[zephyrDescription[pin].pwmChannel];
-	int lowPeriod = pwm_period[zephyrDescription[pin].pwmChannel] - highPeriod;
-	pwm_pin_set_values(pwm_dev, zephyrDescription[pin].pwmChannel, highPeriod, lowPeriod);
+	pwm_pin_set_period(pwm_dev, zephyrDescription[pin].pwmChannel, 
+		pwm_period[zephyrDescription[pin].pwmChannel]);
+	pwm_pin_set_values(pwm_dev, zephyrDescription[pin].pwmChannel, 0,
+		(int)((pwm_period[zephyrDescription[pin].pwmChannel]* HW_CLOCK_CYCLES_PER_USEC) 
+		* (val/(float)maxResolutionValue)));
 }
 
 void analogWriteFrequency(int pin, uint32_t frequency)
 {
-	uint32_t period = F_CPU / frequency;
+	uint32_t period = 1000000 / frequency;
 	pwm_period[zephyrDescription[pin].pwmChannel] = period;
 	
 }
